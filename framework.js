dd8ade66123c0dba1d51867abda8cecd3749baf1
Sandro <41326409+schefbi@users.noreply.github.com>
2022-06-29 11:50:33 +0200

    service to framework

--- framework.js
+++ framework.js
@@ -268,3 +268,14 @@ function renderApps(appElementId) {
     
 }
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('../serviceWorker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+    }
